de4e551d85443c0f3a2f13cee67e7da4a315149a
elad <eladh87@gmail.com>
2016-02-01 15:35:35 +0200

    check destination

--- iltmi.py
+++ iltmi.py
@@ -12,6 +12,11 @@ def move(server, src, dst, direction, pat=None):
     sftp = ssh.open_sftp()
 
     def single_file(src=src, dst=dst):
+        if os.path.isdir(dst) and direction == 'put':
+            exit()
+        if os.path.isdir(dst):
+            dst = dst + '/' + src.split('/')[-1]
+            print dst
         if direction == 'put':
             sftp.put(src, dst)
         elif direction == 'get':
@@ -36,22 +41,33 @@ def main(argv):
     method = 'get'
     pat = None
 
-    for opt, arg in argv:
+    try:
+        opts, args = getopt.getopt(argv, "n:s:d:m:p")
+    except getopt.GetoptError:
+        print 'usage: iltmi.py -n server -s /path/to/file -d /path/to/destination/ [-m [get|put]] [-p regex]'
+        sys.exit(2)
+
+    print opts
+
+    for opt, arg in opts:
+        print opt, arg
         if opt == '-h':
             print "usage: iltmi.py -n server -s /path/to/file -d /path/to/destination/ [-m [get|put]] [-p regex]"
+            # sys.exit(0)
         elif opt == '-n': server = arg
         elif opt == '-s': src = arg
         elif opt == '-d': dst = arg
         elif opt == '-m': method = arg
         elif opt == '-p': pat = arg
 
-    #server = "rnd"
-    #src = "/home/copypaste/dir/"
-    #dst = "/tmp/testdir/"
-    #method = "get"
-    #pat1 = '\.txt$'
+    # server = "rnd"
+    # src = "/home/copypaste/dir/"
+    # dst = "/tmp/testdir/"
+    # method = "get"
+    # pat1 = '\.txt$'
     if pat: pat = r'' + pat + ''
-    move(server, src, dst, method, pat)
+    if server and src and dst and method:
+        move(server, src, dst, method, pat)
 
 if __name__ == "__main__":
     main(sys.argv[1:])
